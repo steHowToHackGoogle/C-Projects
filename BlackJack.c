e8cb3a8aa6f3e4a1996d061e03cf2a9cd429f577
steHowToHackGoogle <giurgistefan1@gmail.com>
2023-07-23 21:38:48 -0700

    2

--- BlackJack.c
+++ BlackJack.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
-int card, value, sum;
+int card1, card2, sum, value1, value2, total;
 char cards[2][13] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-int play()
+int play() {
+    card1 = rand() % 13 + 1;
+    card2 = rand() % 13 + 1;
+
+    if(cards[card1] == "J" || cards[card1] == "Q" || cards[card1] == "K") {
+        value1 = 10;
+    }else {
+        value1 = card1;
+    }
+}
 
 int menu() {
     printf("[Play] annonimously\n[Log] In\n[New] account\n[Exit]\n");
