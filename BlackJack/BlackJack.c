53ee47d77313c6ab6fca8ff8a38af3cd774c4759
steHowToHackGoogle <139350042+steHowToHackGoogle@users.noreply.github.com>
2023-07-31 20:34:29 +0300

    update BlackJack/BlackJack.c

--- BlackJack/BlackJack.c
+++ BlackJack/BlackJack.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <string.h>
 
+int x = 0;
+
 struct contact {
   char number[11];
   char name[30];
@@ -10,10 +12,38 @@ struct contact {
 
 struct contact contacts[100];
 
+void goto_contact() {
+  for(int i = 0; i <= 100; i++) {
+    printf("~~~~~~~~~~\n%s\n%s\n~~~~~~~~~~~\n", contacts[i].name, contacts[i].number);
+  }
+}
+
+void new_contact() {
+  char nname[30];
+  char nnum[30];
+  printf("~~~~~~~~~~\nEnter name: ");
+  gets(nname);
+  printf("~~~~~~~~~~\nEnter number: ");
+  scanf("%s", nnum);
+  strcpy(nname, contacts[x].name);
+  strcpy(nnum, ccontacts[x].nnum);
+  x++;
+  printf("Contact added!");
+}
+
 void menu() {
   char ch;
   printf("~~~~~~~~~~\n(G)o to contacts\n(A)dd new contact\n(E)dit existing contact\n~~~~~~~~~~~~\n");
-  
+  scanf(" %c", ch);
+  if(ch == 'G' || ch == 'g') {
+    goto_contact();
+    menu();
+  }else if(ch == 'A' || ch == 'a') {
+    new_contact();
+    menu();
+  }else if(ch == 'E' || ch == 'e') {
+    
+  }
   
 }
 
